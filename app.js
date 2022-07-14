c8943ebbbb562dc2b7901b323b26fb9619cca044
matheusclima <matheuslima1607@gmail.com>
2022-07-14 09:52:57 -0300

    Instalação das dependencias e inicio do projeto

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+
+let app = express()
+
+app.get("/usuario", (req, res) => {
+
+})
+
+app.post("/usuario", (req, res) => {
+
+})
+
+app.put(`/usuario/:id`, (req, res) => {
+
+})
+
+app.delete(`/usuario/:id`, (req, res) => {
+
+})
