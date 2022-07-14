4edf191fa652b29996fce8c4df6fc7e3d803065f
matheusclima <matheuslima1607@gmail.com>
2022-07-14 17:16:12 -0300

    Criação do método GET no endpoint usuario

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
+// Importar o express
 const express = require("express")
+const db = require("./db.json")
 
-let app = express()
+// Criar uma aplicação/servidor
+const app = express()
 
 app.get("/usuario", (req, res) => {
-
+    let users = JSON.stringify(db, null, 4)
+    res.send(users)
 })
 
 app.post("/usuario", (req, res) => {
-
+    
 })
 
 app.put(`/usuario/:id`, (req, res) => {
@@ -17,3 +21,6 @@ app.put(`/usuario/:id`, (req, res) => {
 app.delete(`/usuario/:id`, (req, res) => {
 
 })
+
+// Fazer o servidor ficar de prontidão aguardando uma requisição
+app.listen(3000, () => {console.log("Servidor rodando na porta 3000")})
